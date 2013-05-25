639a367a99ca9b266b303bfd3c9f8721686789ba
Dosty Everts <dostman@luckynerds.com>
2013-05-24 22:26:02 -0500

    initial commit

---
+++ dat-split/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatSplit::Application.config.secret_token = 'f4e64448e6825d0e81f279847c7f21c6c56793f3f5a7003814a03b3af870ab1e7ef09247b52d05775cefd507d2267be3c9508f872d3bcd52231726d1e511a584'
